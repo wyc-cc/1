02ba3df3fbeb93f73f63dd881c4824e5c6babc2d
wyc-cc <sammywu961114@gmail.com>
2025-03-10 15:05:59 +0800

    Add files via upload

---
+++ dist/dist/script.js
@@ -0,0 +1,8 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count++;
+      counterElement.textContent = count;
+    }
